2ba440bb4afdb34c701314d49d818f47a7451cd0
Ivan Nunez <ivanluis.cba@gmail.com>
2019-05-13 21:21:44 -0300

    Fix password bug in api

--- datient/serializers.py
+++ datient/serializers.py
@@ -2,6 +2,18 @@ from datient.models.doctor import Doctor
 from rest_framework import serializers
 
 class DoctorSerializer(serializers.ModelSerializer):
+    def create(self, validated_data):
+        return Doctor.objects.create_user(**validated_data)
+
+    def update(self, instance, validated_data):
+        for attr, value in validated_data.items():
+            if attr == 'password':
+                instance.set_password(value)
+            else:
+                setattr(instance, attr, value)
+        instance.save()
+        return instance
+
     class Meta:
         model = Doctor
-        fields = '__all__'
+        fields = ('id', 'email', 'password', 'hierarchy', 'first_name', 'last_name', 'created_at')
