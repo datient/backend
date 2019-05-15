521c9761c50f192d3a4844239ef735537439b97d
Ivan Nuñez <ivanluis.cba@gmail.com>
2019-05-15 15:15:48 -0300

    Change patient model fields

--- datient/models/patient.py
+++ datient/models/patient.py
@@ -1,4 +1,4 @@
-from django.core.validators import MaxValueValidator
+from django.core.validators import MaxValueValidator, MinValueValidator
 from django.db import models
 
 SEXES = (
@@ -7,11 +7,19 @@ SEXES = (
 )
 
 class Patient(models.Model):
-    dni = models.CharField(max_length=8, unique=True)
-    first_name = models.CharField(max_length=40)
-    last_name = models.CharField(max_length=40)
-    history_number = models.PositiveIntegerField(blank=True)
+    dni = models.PositiveIntegerField(primary_key=True, unique=True,
+        validators=[
+            MinValueValidator(10000000),
+            MaxValueValidator(99999999)
+        ]
+    )
+    first_name = models.CharField(max_length=40, blank=True)
+    last_name = models.CharField(max_length=40, blank=True)
+    history_number = models.PositiveIntegerField(blank=True, null=True)
     sex = models.PositiveSmallIntegerField(choices=SEXES, validators=[MaxValueValidator(1)])
-    income_diagnosis = models.TextField()
+    income_diagnosis = models.TextField(blank=True)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return f'{self.dni}'
