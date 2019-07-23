4927e34d44fc18378bbb08b7b7713355552ae751
Ivan Nuñez <ivanluis.cba@gmail.com>
2019-07-23 13:09:22 -0300

    Add create function to hospitalization serializer

--- datient/serializers/hostpital.py
+++ datient/serializers/hostpital.py
@@ -1,7 +1,7 @@
 from rest_framework import serializers
 
 from . import ProgressSerializer
-from datient.models import Hospitalization
+from datient.models import Hospitalization, Progress
 
 
 class HospitalizationSerializer(serializers.ModelSerializer):
@@ -12,3 +12,10 @@ class HospitalizationSerializer(serializers.ModelSerializer):
         model = Hospitalization
         fields = ('id', 'entry_at', 'left_at', 'done_at',
                   'bed', 'doctor', 'patient', 'progress')
+
+    def create(self, validated_data):
+        progress_data = validated_data.pop('progress')
+        progress = Progress.objects.create(**progress_data)
+        hospitalization = Hospitalization.objects.create(progress=progress,
+                                                         **validated_data)
+        return hospitalization
