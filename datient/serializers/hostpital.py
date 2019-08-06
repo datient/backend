c14a87631f988357a783fa2b8caffe9a012e3bba
Ivan Nuñez <ivan.nunez@25watts.com.ar>
2019-08-06 10:13:48 -0300

    Add bording days field in api

--- datient/serializers/hostpital.py
+++ datient/serializers/hostpital.py
@@ -1,3 +1,4 @@
+from django.utils.timezone import now
 from rest_framework import serializers
 
 from . import ProgressSerializer
@@ -6,12 +7,14 @@ from datient.models import Hospitalization, Progress
 
 class HospitalizationSerializer(serializers.ModelSerializer):
 
+    boarding_days = serializers.SerializerMethodField()
     progress = ProgressSerializer()
 
     class Meta:
         model = Hospitalization
         fields = ('id', 'entry_at', 'left_at', 'done_at',
-                  'bed', 'doctor', 'patient', 'progress')
+                  'bed', 'doctor', 'patient', 'boarding_days',
+                  'progress')
 
     def create(self, validated_data):
         progress_data = validated_data.pop('progress')
@@ -19,3 +22,9 @@ class HospitalizationSerializer(serializers.ModelSerializer):
         hospitalization = Hospitalization.objects.create(progress=progress,
                                                          **validated_data)
         return hospitalization
+
+    def get_boarding_days(self, obj):
+        if obj.entry_at is not None:
+            return (now() - obj.entry_at).days
+        
+        return None
