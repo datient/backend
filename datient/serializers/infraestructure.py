7f1d6652b3527a18332c3ab541ecc59213dc2778
Ivan Nuñez <ivanluis.cba@gmail.com>
2019-10-08 11:22:57 -0300

    Add is available field to room api

--- datient/serializers/infraestructure.py
+++ datient/serializers/infraestructure.py
@@ -1,14 +1,26 @@
 from rest_framework import serializers
 
-from . import HospitalizationSerializer
-from datient.models import Bed, Room
+from datient.models import Bed, Hospitalization, Room
 
 
 class BedSerializer(serializers.ModelSerializer):
 
+    is_available = serializers.SerializerMethodField()
+
     class Meta:
         model = Bed
-        fields = ('id', 'name', 'room')
+        fields = ('id', 'name', 'room', 'is_available')
+
+    def get_is_available(self, obj):
+        try:
+            hosp = Hospitalization.objects.filter(bed__id=obj.id).order_by('done_at').last()
+            dni = hosp.patient.dni
+            hosp2 = Hospitalization.objects.filter(patient__dni=dni).order_by('done_at').last()
+            if (hosp.id == hosp2.id and hosp2.left_at is None):
+                return False
+            return True
+        except:
+            return True
 
 
 class RoomSerializer(serializers.ModelSerializer):
