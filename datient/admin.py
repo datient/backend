0834dba20faf293bf0f0f798244c78104d3962fd
Ivan Nuñez <ivanluis.cba@gmail.com>
2019-10-23 13:19:34 -0300

    Give model admin to all models

--- datient/admin.py
+++ datient/admin.py
@@ -1,13 +1,12 @@
 from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin
 from django.contrib.auth.models import Group
-from datient.models.doctor import Doctor
-from datient.models.hospital import Hospitalization
-from datient.models.infraestructure import Bed, Room
-from datient.models.patient import (Patient, ComplementaryStudy,
-                                    Progress, FuturePlan)
+
+from datient.models import (Bed, Doctor, ComplementaryStudy, FuturePlan,
+                            Hospitalization, Patient, Progress, Room)
 
 
+@admin.register(Doctor)
 class DoctorAdmin(UserAdmin):
 
     list_display = ('email', 'full_name', 'hierarchy')
@@ -27,28 +26,54 @@ class DoctorAdmin(UserAdmin):
     filter_horizontal = ()
 
 
+@admin.register(Bed)
+class BedAdmin(admin.ModelAdmin):
+
+    list_display = ('id', 'name')
+
+
 class BedInLine(admin.TabularInline):
 
     model = Bed
 
 
+@admin.register(ComplementaryStudy)
+class ComplementaryStudyAdmin(admin.ModelAdmin):
+
+    list_display = ('id', 'image', 'patient')
+
+
+@admin.register(FuturePlan)
+class FuturePlanAdmin(admin.ModelAdmin):
+
+    list_display = ('id', 'title', 'description', 'patient')
+
+
+@admin.register(Hospitalization)
+class HospitalizationAdmin(admin.ModelAdmin):
+
+    list_display = ('id', 'doctor', 'patient', 'bed', 'entry_at', 'done_at')
+
+
+@admin.register(Patient)
+class PatientAdmin(admin.ModelAdmin):
+
+    list_display = ('dni', 'full_name', 'age', 'gender')
+    list_filter = ('gender',)
+
+
+@admin.register(Room)
 class RoomAdmin(admin.ModelAdmin):
 
     inlines = [BedInLine]
     ordering = ('pk',)
 
 
-class HospitalizationAdmin(admin.ModelAdmin):
+@admin.register(Progress)
+class ProgressAdmin(admin.ModelAdmin):
 
-    list_display = ('doctor', 'patient', 'bed', 'done_at')
+    list_display = ('id', 'diagnosis', 'status', 'patient')
+    list_filter = ('status',)
 
 
-admin.site.register(Bed)
-admin.site.register(Doctor, DoctorAdmin)
-admin.site.register(Hospitalization, HospitalizationAdmin)
-admin.site.register(Room, RoomAdmin)
-admin.site.register(Patient)
-admin.site.register(ComplementaryStudy)
-admin.site.register(Progress)
-admin.site.register(FuturePlan)
 admin.site.unregister(Group)
